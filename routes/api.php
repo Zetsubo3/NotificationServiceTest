b397226ab15df004d9a0c3851037fe49f8747780
Zetsubo <zetsubo3@yandex.ru>
2026-05-18 12:47:46 +0300

    auth_commit

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,9 @@ use App\Http\Controllers\API\NotificationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/send', [NotificationController::class, 'send']);
+    Route::get('/history/{recipientId}', [NotificationController::class, 'history']);
+});
 
 
-
-Route::post('/send', [NotificationController::class, 'send']);
-Route::get('/history/{recipientId}', [NotificationController::class, 'history']);
